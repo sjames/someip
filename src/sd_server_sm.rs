6847cb582b8356999b5d6330f5562ef73be7fcb0
Sojan James <sojan.james@gmail.com>
2021-06-19 22:44:54 +0530

    Implemented all transitions and tested happy path

--- src/sd_server_sm.rs
+++ src/sd_server_sm.rs
@@ -2,6 +2,7 @@ use crate::server;
 use rand::prelude::*;
 
 ///Events that can be injected into the state machine
+
 pub trait State {
     fn on_entry(&mut self) {}
     fn on_exit(&mut self) {}
@@ -25,9 +26,13 @@ struct SMData {
     interface_status: bool,
     service_status: bool,
     set_timer: Box<dyn FnMut(u32, std::time::Duration)>,
-    reset_timer: Box<dyn FnMut(u32)>,
+    clear_timer: Box<dyn FnMut(u32)>,
+    send_offer: Box<dyn FnMut()>,
     initial_delay_min: std::time::Duration,
     initial_delay_max: std::time::Duration,
+    repetitions_max: u32,
+    repetitions_base_delay: std::time::Duration,
+    cyclic_announce_delay: std::time::Duration,
 }
 
 impl SMData {
@@ -54,9 +59,13 @@ pub enum SDServerStateMachine {
 impl SDServerStateMachine {
     pub fn new(
         set_timer: Box<dyn FnMut(u32, std::time::Duration)>,
-        reset_timer: Box<dyn FnMut(u32)>,
+        clear_timer: Box<dyn FnMut(u32)>,
+        send_offer: Box<dyn FnMut()>,
         initial_delay_min: std::time::Duration,
         initial_delay_max: std::time::Duration,
+        repetitions_max: u32,
+        repetitions_base_delay: std::time::Duration,
+        cyclic_announce_delay: std::time::Duration,
     ) -> Self {
         SDServerStateMachine::NotReady(Data::<NotReady> {
             state: NotReady::default(),
@@ -64,9 +73,13 @@ impl SDServerStateMachine {
                 interface_status: false,
                 service_status: false,
                 set_timer,
-                reset_timer,
+                clear_timer,
+                send_offer,
                 initial_delay_min,
                 initial_delay_max,
+                repetitions_max,
+                repetitions_base_delay,
+                cyclic_announce_delay,
             },
         })
     }
@@ -120,6 +133,30 @@ impl From<Data<RepetitionPhase>> for Data<NotReady> {
     }
 }
 
+impl From<Data<MainPhase>> for Data<NotReady> {
+    fn from(mut d: Data<MainPhase>) -> Self {
+        d.state.on_exit();
+        let mut state = NotReady::default();
+        state.on_entry();
+        Self {
+            inner: d.inner,
+            state,
+        }
+    }
+}
+
+impl From<Data<RepetitionPhase>> for Data<MainPhase> {
+    fn from(mut d: Data<RepetitionPhase>) -> Self {
+        d.state.on_exit();
+        let mut state = MainPhase::default();
+        state.on_entry();
+        Self {
+            inner: d.inner,
+            state,
+        }
+    }
+}
+
 impl State for SDServerStateMachine {
     fn next(mut self, event: SMEvent) -> Self
     where
@@ -163,6 +200,7 @@ impl State for SDServerStateMachine {
                 if d.inner.interface_status && d.inner.service_status {
                     SDServerStateMachine::InitialWaitPhase(d)
                 } else {
+                    (d.inner.clear_timer)(0);
                     SDServerStateMachine::NotReady(d.into())
                 }
             }
@@ -175,13 +213,15 @@ impl State for SDServerStateMachine {
                 if d.inner.interface_status && d.inner.service_status {
                     SDServerStateMachine::InitialWaitPhase(d)
                 } else {
+                    (d.inner.clear_timer)(0);
                     SDServerStateMachine::NotReady(d.into())
                 }
             }
             //Timeout in Initial wait phase. Timer id should be 0
-            (SDServerStateMachine::InitialWaitPhase(d), SMEvent::Timeout(tid)) => {
+            (SDServerStateMachine::InitialWaitPhase(mut d), SMEvent::Timeout(tid)) => {
                 if tid == 0 {
                     //todo: SendOffer service
+                    (d.inner.set_timer)(1, d.inner.repetitions_base_delay);
                     SDServerStateMachine::RepetitionPhase(d.into())
                 } else {
                     println!("Unexpected timer id for timeout:{}", tid);
@@ -204,13 +244,63 @@ impl State for SDServerStateMachine {
                     SDServerStateMachine::RepetitionPhase(d)
                 }
             }
-            (SDServerStateMachine::RepetitionPhase(_), SMEvent::ServiceConfiguration(_)) => todo!(),
-            (SDServerStateMachine::RepetitionPhase(_), SMEvent::Timeout(_)) => todo!(),
-            (SDServerStateMachine::RepetitionPhase(_), SMEvent::FindService) => todo!(),
-            (SDServerStateMachine::MainPhase(_), SMEvent::IfStatusChanged(_)) => todo!(),
-            (SDServerStateMachine::MainPhase(_), SMEvent::ServiceConfiguration(_)) => todo!(),
-            (SDServerStateMachine::MainPhase(_), SMEvent::Timeout(_)) => todo!(),
-            (SDServerStateMachine::MainPhase(_), SMEvent::FindService) => todo!(),
+            (
+                SDServerStateMachine::RepetitionPhase(mut d),
+                SMEvent::ServiceConfiguration(enabled),
+            ) => {
+                d.inner.service_status = enabled;
+                if !enabled {
+                    SDServerStateMachine::NotReady(d.into())
+                } else {
+                    SDServerStateMachine::RepetitionPhase(d)
+                }
+            }
+            (SDServerStateMachine::RepetitionPhase(mut d), SMEvent::Timeout(tid)) => {
+                (d.inner.send_offer)();
+                d.state.run += 1;
+                assert!(tid == 1);
+                if d.state.run > d.inner.repetitions_max {
+                    (d.inner.set_timer)(2, d.inner.cyclic_announce_delay);
+                    SDServerStateMachine::MainPhase(d.into())
+                } else {
+                    (d.inner.set_timer)(1, d.inner.repetitions_base_delay);
+                    SDServerStateMachine::RepetitionPhase(d)
+                }
+            }
+            (SDServerStateMachine::RepetitionPhase(mut d), SMEvent::FindService) => {
+                (d.inner.clear_timer)(1); // reset the timer
+                (d.inner.send_offer)();
+                (d.inner.set_timer)(1, d.inner.repetitions_base_delay);
+                SDServerStateMachine::RepetitionPhase(d)
+            }
+            (SDServerStateMachine::MainPhase(mut d), SMEvent::IfStatusChanged(if_enabled)) => {
+                d.inner.interface_status = if_enabled;
+                if !if_enabled {
+                    SDServerStateMachine::NotReady(d.into())
+                } else {
+                    SDServerStateMachine::MainPhase(d)
+                }
+            }
+            (SDServerStateMachine::MainPhase(mut d), SMEvent::ServiceConfiguration(enabled)) => {
+                d.inner.service_status = enabled;
+                if !enabled {
+                    SDServerStateMachine::NotReady(d.into())
+                } else {
+                    SDServerStateMachine::MainPhase(d)
+                }
+            }
+            (SDServerStateMachine::MainPhase(mut d), SMEvent::Timeout(tid)) => {
+                assert!(tid == 2);
+                (d.inner.send_offer)();
+                (d.inner.set_timer)(2, d.inner.cyclic_announce_delay);
+                SDServerStateMachine::MainPhase(d)
+            }
+            (SDServerStateMachine::MainPhase(mut d), SMEvent::FindService) => {
+                (d.inner.clear_timer)(2); // reset the timer
+                (d.inner.send_offer)();
+                (d.inner.set_timer)(2, d.inner.repetitions_base_delay);
+                SDServerStateMachine::MainPhase(d)
+            }
         }
     }
 }
@@ -223,10 +313,13 @@ impl State for NotReady {}
 pub struct InitialWaitPhase {}
 impl State for InitialWaitPhase {}
 #[derive(Default, Debug)]
-pub struct RepetitionPhase {}
+pub struct RepetitionPhase {
+    run: u32,
+}
 impl State for RepetitionPhase {}
 #[derive(Default, Debug)]
 pub struct MainPhase {}
+impl State for MainPhase {}
 
 #[cfg(test)]
 mod test {
@@ -240,8 +333,14 @@ mod test {
             Box::new(|timer_id| {
                 println!("Resetting timer {} ", timer_id);
             }),
+            Box::new(|| {
+                println!("Send Offer");
+            }),
             std::time::Duration::from_millis(10),
             std::time::Duration::from_millis(100),
+            3,
+            std::time::Duration::from_secs(1),
+            std::time::Duration::from_secs(5),
         );
         assert!(matches!(server_sm, SDServerStateMachine::NotReady(_)));
         let server_sm = server_sm.next(SMEvent::ServiceConfiguration(true));
@@ -253,11 +352,28 @@ mod test {
         ));
         let server_sm = server_sm.next(SMEvent::IfStatusChanged(false));
         assert!(matches!(server_sm, SDServerStateMachine::NotReady(_)));
+        let server_sm = server_sm.next(SMEvent::IfStatusChanged(true));
+        assert!(matches!(
+            server_sm,
+            SDServerStateMachine::InitialWaitPhase(_)
+        ));
         server_sm
     }
 
     #[test]
-    fn test_ctor() {
+    fn test_happy_path() {
         let sm = create_ready_sm();
+        let sm = sm.next(SMEvent::Timeout(0));
+        assert!(matches!(sm, SDServerStateMachine::RepetitionPhase(_)));
+        let sm = sm.next(SMEvent::Timeout(1));
+        assert!(matches!(sm, SDServerStateMachine::RepetitionPhase(_)));
+        let sm = sm.next(SMEvent::Timeout(1));
+        let sm = sm.next(SMEvent::Timeout(1));
+        let sm = sm.next(SMEvent::Timeout(1));
+        assert!(matches!(sm, SDServerStateMachine::MainPhase(_)));
+        let sm = sm.next(SMEvent::Timeout(2));
+        assert!(matches!(sm, SDServerStateMachine::MainPhase(_)));
+        let sm = sm.next(SMEvent::IfStatusChanged(false));
+        assert!(matches!(sm, SDServerStateMachine::NotReady(_)));
     }
 }
