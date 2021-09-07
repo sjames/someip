2c641f6998b7698907e2a8dfa32651a79fc01b75
Sojan James <sojan.james@gmail.com>
2021-09-07 17:13:46 +0530

    License

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,21 @@
-// A SOME/IP server (provider)
+/*
+    Copyright 2021 Sojan James
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+// A SOME/IP server
 
 use async_trait::async_trait;
 use futures::future::BoxFuture;
 use std::{io, net::SocketAddr, sync::Arc};
-use tokio::net::UnixStream;
 use tokio::sync::mpsc::channel;
 use tokio::sync::mpsc::{Receiver, Sender};
 
